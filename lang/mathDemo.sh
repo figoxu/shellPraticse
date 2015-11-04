bdfd770a348c1d7b217db129c926eb952f9ad56f
xujh <xujianhui@mrocker.com>
2015-11-04 10:46:59 +0800

    demo of math

--- lang/mathDemo.sh
+++ lang/mathDemo.sh
@@ -1,7 +1,20 @@
 #!/bin/bash 
 
+echo -e "\e[1;31m math demo of 'let' \e[0m"
 x=1999 
 let "x = $x + 1" 
-echo $x 
+echo $x
+let "x = $x + 100"
+echo $x
+let "x +=1"
+echo $x
+let "x -=1"
+echo $x
+let x-=1
+echo $x
+let x+=1
+echo $x
+let x=x+99
+echo $x
 x="olympic'"$x 
 echo $x
