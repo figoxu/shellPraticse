9746a398d872a832ff02802d0d5603eda5be4cc3
xujh <xujianhui@mrocker.com>
2015-12-30 15:56:35 +0800

    lib of shell invoke

---
+++ importSample/libs/libMethod.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+function libMethod()
+{
+	echo "lib Method invoke...."
+	echo "shell name :" $0 "param is "  $1
+}
---
+++ importSample/sample.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+p=`pwd`/libs/libMethod.sh
+source $p
+echo $p
+
+echo "my lib invoke demo"
+libMethod "cool"
